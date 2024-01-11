dc8d583b5856b0fcd63cf6d88f9f10c2d39b7b45
Bantamlak <bantamlak29@gmail.com>
2024-01-11 10:13:39 +0300

    solution to task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A module with basic annotations - to string"""
+
+
+def to_str(n: float) -> str:
+    """ A ype-annotated function `to_str` that takes a float `n` as
+        argument and returns the string representation of the float.
+    """
+    return str(n)
