7edf15927f8856b7323da11e1db616f975065012
Bantamlak <bantamlak29@gmail.com>
2024-01-11 14:04:08 +0300

    solution to task 10

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""A module that returns the fist element of a sequence"""
+from typing import Sequence, Any, Union
+
+NoneType = type(None)
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, NoneType]:
+    """
+    A function that returns the first element of a sequence.
+
+    paramenters:
+    - lst (lst: Sequence[Any]): A sequence where the first element is to be
+    retrieved.
+
+    Returns:
+    - Union[Any, NoneType]: The first element of the sequence if it does exist,
+    else return None
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
