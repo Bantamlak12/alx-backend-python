21e275ffe388b4f01298ed6c24c268d8c51648cc
Bantamlak <bantamlak29@gmail.com>
2024-01-11 09:35:45 +0300

    solution to task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A module with basic annotations - concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """ Accepts  `str1` and `str2` as a string, and
+        returns a concatenated string.
+    """
+    return str1 + str2
