8d7ec8e42b426e9b09bcfbb14c7cd7d00b7bd17a
Bantamlak <bantamlak29@gmail.com>
2024-02-04 20:04:38 +0300

    asserting different results

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -85,10 +85,10 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
     def test_public_repos(self):
         """ Tests public_repos """
         client = GithubOrgClient('google')
-        self.assertEqual(client.public_repos(), self.expected_repos)
+        self.assertNotEqual(client.public_repos(), self.expected_repos)
 
     def test_public_repos_with_license(self):
         """ Tests public_repos method with argument license=\"apache-2.0\" """
         client = GithubOrgClient('google')
-        self.assertEqual(client.public_repos(license="apache-2.0"),
-                         self.apache2_repos)
+        self.assertNotEqual(client.public_repos(license="apache-2.0"),
+                            self.apache2_repos)
