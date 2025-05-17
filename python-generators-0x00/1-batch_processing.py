fbd8552ca1c29a2db9daa1ee113acd4eabb909f4
Bantamlak <neoluka315@gmail.com>
2025-05-17 17:22:45 +0300

    batch processing

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,32 @@
+import mysql.connector
+import mysql.connector.errorcode
+
+def create_connection():
+    """Creates connection to the database"""
+    return mysql.connector.connect(
+        host="localhost",
+        user="bantamlak",
+        password="dispensed",
+        database="ALX_prodev"
+    )
+
+def stream_users_in_batches(batch_size):
+    connection = create_connection()
+    cursor = connection.cursor()
+
+    cursor.execute("SELECT * FROM user_data")
+
+    while True:
+        batch = cursor.fetchmany(batch_size)
+        if not batch:
+            break
+        yield batch
+
+    cursor.close()
+    connection.close()
+
+def batch_processing(batch_size):
+    for batch in stream_users_in_batches(batch_size):
+        filtered_batch = [user for user in batch if user[3] > 25]
+        if filtered_batch:
+            print(filtered_batch, "\n")
