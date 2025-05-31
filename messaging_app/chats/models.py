767eee9c89c04d099aac869eafa44d9ff08fccdd
Bantamlak <neoluka315@gmail.com>
2025-05-31 13:28:58 +0300

    models updated as per the requirements

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,24 +1,28 @@
 """ models.py: Defines custom user, conversations, and messaging models.
 """
+import uuid
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class CustomUser(AbstractUser):
     """A user model which is an extension of the Abstract user."""
-    profile_picture = models.ImageField(upload_to='profile_pictures/', blank=True, null=True)
+    user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    phone_number = models.CharField(max_length=15, blank=True,null=True)
 
 
-class Coversation(models.Model):
+
+class Conversation(models.Model):
     """A model that tracks which users are involved in a conversation."""
-    participants = models.ManyToManyField('CustomUser', related_name='conversations')
+    conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(CustomUser, related_name='conversations')
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Message(models.Model):
     """ A model that define messaging between users."""
-    conversation = models.ForeignKey(Coversation, related_name='messages', on_delete=models.CASCADE)
-    sender = models.ForeignKey('CustomUser', related_name='sent_message', on_delete=models.CASCADE)
-    content = models.TextField()
-    is_read = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True)
+    message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='sent-messages+')
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    message_body = models.TextField()
+    sent_at = models.DateTimeField(auto_now_add=True)
