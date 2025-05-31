3961d69caa81126a6c143bd57a5d6f96b415514a
Bantamlak <neoluka315@gmail.com>
2025-05-31 10:34:13 +0300

    default authentication class had typo and corrected

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -43,10 +43,10 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASS': [
+    'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated'
     ],
-    'DEFAULT_AUTHENTICATION_CLASS': [
+    'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
     ]
 }
