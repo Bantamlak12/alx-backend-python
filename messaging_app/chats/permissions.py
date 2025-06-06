e643879492531d97f89157a8dbad8b259031640a
Bantamlak <neoluka315@gmail.com>
2025-06-07 00:51:48 +0300

    permission class updated

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,17 +1,19 @@
 from rest_framework import permissions
 
-class IsParticipantOrReadOnly(permissions.BasePermission):
-    """Allow access only to participants of a conversation"""
 
+class IsParticipantOrReadOnly(permissions.BasePermission):
+    """Allow read-only access for everyone, but restric to participants of a conversation"""
     def has_object_permission(self, request, view, obj):
+        if request.method not in ['PUT', 'PATCH', 'DELETE']:
+            return True
         return request.user in obj.participants.all()
 
 
 class IsParticipantOfConversation(permissions.BasePermission):
     """Allow only participants in a conversation to access messages"""
-    def has_object_permission(self, request, view, obj):
-        if hasattr(obj, 'participants'):
-            return request.user in obj.participants.all()
-        elif hasattr(obj, 'conversation'):
-            return request.user in obj.conversation.participants.all()
+    def has_object_permission(self, request, view, obj):        
+        # Check if the user is participant of the conversation
+        if hasattr(obj, 'conversation') and user in obj.conversation.participants.all():
+            return True
+
         return False
