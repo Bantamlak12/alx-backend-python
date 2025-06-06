c2338a79f615211f60f207f7a0f4ff3fd03c0bc0
Bantamlak <neoluka315@gmail.com>
2025-06-06 23:13:56 +0300

    custom auth logic implementation

---
+++ messaging_app/chats/auth.py
@@ -0,0 +1,9 @@
+from rest_framework_simplejwt.tokens import RefreshToken
+
+def get_token_user(user):
+    """Generate JWT tokens for a given user"""
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
